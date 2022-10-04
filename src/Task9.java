56a1efcdb71300539620966c7300508d9cdbb70c
bahodurotayev <tjkbaha@gmail.com>
2022-10-04 17:07:42 -0400

    Task#7

--- src/Task9.java
+++ src/Task9.java
@@ -15,15 +15,28 @@ public class Task9 {
                 arr[i] = arr[i - 1];
                 arr[i - 1] = temp;
             }
-
         }
         return arr;
+    }
+
+    static int[] rotateArray2 (int[] arr, int k){
+
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < k ; i++) {
+          newArr[i] = arr[arr.length - i-1];
+        }
+        int j = 0;
+        for (int i = k; i <arr.length; i++) {
+            newArr[i]= arr[j];
+            j++;
+        }
+        return newArr;
     }
 
     public static void main(String[] args) {
 
         int[]num = {1, 2, 3, 4, 5, 6, 7};
-        int[] ints = rotateArray(num, 3);
+        int[] ints = rotateArray2(num, 3);
         System.out.println("ints = " + Arrays.toString(ints));
 
 
