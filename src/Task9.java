8fb15cae2cd1c09080e899aa4209093b09b86ce8
bahodurotayev <tjkbaha@gmail.com>
2022-10-03 23:04:17 -0400

    Task#7

---
+++ src/Task9.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Task9 {
+
+
+    /* Rotate array in Java
+             Rotate an array of n elements to the right by k steps. For example, with
+             n = 7 and k = 3, the array [1,2,3,4,7,5,6] is rotated to [7,6,5,1,2,3,4].*/
+
+    static int[] rotateArray (int[] arr, int k){
+        for (int j = 0; j < k; j++) {
+
+            for (int i = arr.length - 1; i > j; i--) {
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        int[]num = {1, 2, 3, 4, 5, 6, 7};
+        int[] ints = rotateArray(num, 3);
+        System.out.println("ints = " + Arrays.toString(ints));
+
+
+    }
+}
