6cef762ccbfb01a2f5d56849665cbf31fd9d7119
bahodurotayev <tjkbaha@gmail.com>
2022-10-06 18:46:18 -0400

    Task#12

--- src/Task12.java
+++ src/Task12.java
@@ -1,2 +1,58 @@
+import java.util.Arrays;
+
 public class Task12 {
+
+    static int[] sortedArr (int[] arr){
+
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i] > arr[i + 1]){
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i+1] = temp;
+                i = 0;
+            }
+
+        }
+        return arr;
+    }
+    static int[] sortedArr2 (int[] arr){
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i; j <arr.length ; j++) {
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    }
+                }
+            }
+        return arr;
+    }
+
+    static int[] sumOfTwo(int[]arr, int target){
+        int a =0;
+        int b = arr.length-1;
+        while (a <= b){
+            int sum = arr[a] + arr[b];
+            if(sum > target){
+                b -= 1;
+            } else if (sum < target) {
+                a += 1;
+            }
+            else {
+                return new int[]{a + 1, b + 1};
+            }
+
+        }
+        return  new int[]{a + 1, b + 1};
+    }
+
+
+    public static void main(String[] args) {
+        int [] arr = {2, 11, 7, 15};
+        sortedArr(arr);
+        int[] ints = sumOfTwo(arr, 9);
+        System.out.println(Arrays.toString(ints));
+
+    }
 }
