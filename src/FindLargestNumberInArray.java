ce194254d109becf0ac296e51a547a5220c3cfd0
bahodurotayev <tjkbaha@gmail.com>
2022-10-13 12:32:44 -0400

    Task#10

---
+++ src/FindLargestNumberInArray.java
@@ -0,0 +1,17 @@
+public class FindLargestNumberInArray {
+    static int findLargestNumberInArray(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[]arr= {2, 3, 5, 1, 5, 7, 10};
+        int largestNumberInArray = findLargestNumberInArray(arr);
+        System.out.println("largestNumberInArray = " + largestNumberInArray);
+    }
+}
---
+++ src/FindSmallestNumberInArray.java
@@ -0,0 +1,17 @@
+public class FindSmallestNumberInArray {
+    static int findSmallestNumberArray(int[] arr){
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 6, 2, 6, 8, 12, 0};
+        int smallestNumberArray = findSmallestNumberArray(arr);
+        System.out.println("smallestNumberArray = " + smallestNumberArray);
+    }
+}
