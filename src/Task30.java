7b80273b8d3a46b08aa5e9eb34e8235bcb363e54
bahodurotayev <tjkbaha@gmail.com>
2022-10-13 21:26:03 -0400

    Task#10

--- src/Task30.java
+++ src/Task30.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Task30 {
     static int nonRepeatingCharsIndex(String str) {
@@ -20,8 +17,8 @@ public class Task30 {
     return result;
     }
     public static void main(String[] args) {
-        String str = "momo";
-        int i = nonRepeatingCharsIndex3(str);
+        String str = "mom";
+        int i = nonRepeatingCharsIndex4(str);
         System.out.println(i);
     }
     static int nonRepeatingCharsIndex2(String str) {
@@ -53,5 +50,17 @@ public class Task30 {
             }
         }
         return min == Integer.MAX_VALUE ? - 1 : min;
+    }
+    static int nonRepeatingCharsIndex4(String str){
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0)+1);
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if(map.get(str.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
     }
 }
