b036ccff1936630b10dea61ed44c69113fbec835
bahodurotayev <tjkbaha@gmail.com>
2022-10-16 11:30:15 -0400

    Task#34

--- src/Task28.java
+++ src/Task28.java
@@ -14,12 +14,26 @@ public class Task28 {
       return profit;
     }
     static int bestTimeToBuyAndSellStock2(int[] prices){
-      return -1;
+      int min = Integer.MAX_VALUE;
+      int profit = 0;
+      int index = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if(prices[i] < min){
+                min = prices[i];
+                index = i;
+            }
+        }
+        for (int i = index; i < prices.length; i++) {
+            if(prices[i] - min > profit){
+                profit = prices[i] - min;
+            }
+        }
+        return profit;
     }
     public static void main(String[] args) {
 
-       int[] prices = {1,7,4,3,1}; // 7,4,5,3,6,1    7,1,5,3,6,4  7,6,4,3,1  1,7,4,3,1
-        int i = bestTimeToBuyAndSellStock(prices);
+       int[] prices = {7,6,4,3,1}; // 7,4,5,3,6,1    7,1,5,3,6,4  7,6,4,3,1  1,7,4,3,1
+        int i = bestTimeToBuyAndSellStock2(prices);
         System.out.println(i);
     }
 
