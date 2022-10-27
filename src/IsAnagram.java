d2d4efa32834fe27993201b553f4d8934823395c
bahodurotayev <tjkbaha@gmail.com>
2022-10-27 19:00:13 -0400

    breakSpace

---
+++ src/IsAnagram.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class IsAnagram {
+    public static void main(String[] args) {
+        String a = "mom";
+        String b = "mam";
+
+        char[] achar = a.toCharArray();
+        char[] bchar = b.toCharArray();
+
+        Arrays.sort(achar);
+        Arrays.sort(bchar);
+
+        boolean isAnagram = false;
+        for (int i = 0; i < achar.length; i++) {
+            if(achar[i] == bchar[i]) {
+                isAnagram = true;
+            }
+        }
+        System.out.println(isAnagram);
+    }
+}
